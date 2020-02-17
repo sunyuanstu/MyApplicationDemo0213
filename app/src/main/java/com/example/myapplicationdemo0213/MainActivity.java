4cb07298f0585d4ef33c9d6bbb2acb705eedfd1c
MAX <MAX@gmail.com>
2020-02-17 22:12:15 +0800

    我是0213里面的project

--- app/src/main/java/com/example/myapplicationdemo0213/MainActivity.java
+++ app/src/main/java/com/example/myapplicationdemo0213/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.activity_main_button1:
                 Log.d(TAG, "onClick: activity_main_button1");
+                activity_main_textview.setText("我是0213里面的project");
                 break;
             case R.id.activity_main_button2:
                 Log.d(TAG, "onClick: activity_main_button2");
