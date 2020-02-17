5e889a3692f5a18885681649bf11b6db6ed241ef
MAX <MAX@gmail.com>
2020-02-17 21:57:25 +0800

    在readme里面更改

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MyApplicationDemo0213
 提交到Github的测试Demo
+在0217里面更改
--- app/src/main/java/com/example/myapplicationdemo0213/MainActivity.java
+++ app/src/main/java/com/example/myapplicationdemo0213/MainActivity.java
@@ -32,8 +32,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.activity_main_button1:
                 Log.d(TAG, "onClick: activity_main_button1");
-                activity_main_textview.setText("button1点击了我");
-                activity_main_textview.setText("我被button1点击了");
                 break;
             case R.id.activity_main_button2:
                 Log.d(TAG, "onClick: activity_main_button2");
